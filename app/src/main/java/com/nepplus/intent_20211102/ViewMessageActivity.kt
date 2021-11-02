c8cd629aea3883993e07cc1d047acd36615a1ea4
김민성 <kmsz5813@naver.com>
2021-11-02 17:41:17 +0900

    넘어간 화면에서 -> 첨부한 string을 받아서 -> 텍스트 뷰에 반영

--- app/src/main/java/com/nepplus/intent_20211102/ViewMessageActivity.kt
+++ app/src/main/java/com/nepplus/intent_20211102/ViewMessageActivity.kt
@@ -2,10 +2,16 @@ package com.nepplus.intent_20211102
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_view_message.*
 
 class ViewMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_message)
+
+        val receiveMessage = intent.getStringArrayExtra("message")
+
+        txtMessage.text = receiveMessage
+
     }
 }
