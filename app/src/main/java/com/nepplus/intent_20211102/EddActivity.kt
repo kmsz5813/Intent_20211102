2c463061aa710657b1e25567710a6024a4bad60f
김민성 <kmsz5813@naver.com>
2021-11-03 09:57:03 +0900

    닉네임 입력화면 추가

---
+++ app/src/main/java/com/nepplus/intent_20211102/EddActivity.kt
@@ -0,0 +1,11 @@
+package com.nepplus.intent_20211102
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EddActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edd)
+    }
+}
